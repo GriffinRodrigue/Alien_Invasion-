3d4f1aa0b7667f3e61748e31e4e9e0fc789e7f03
Griffin Rodrigue <47837201+GriffinRodrigue@users.noreply.github.com>
2020-04-14 15:09:20 -0400

    cant get high score to stay
    moving on to a new project

--- game_stats.py
+++ game_stats.py
@@ -8,10 +8,10 @@ class GameStats:
         #start alien invasion in an inactive state.
         self.game_active = True
         #high score should never be reset.
-        # self.high_score = 0
+        self.high_score = 0
     def reset_stats(self):
         """"Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
-        self.high_score = 0
+       
